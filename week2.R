15e04bb79131ea7e76c28062612b1671277d6efb
Payam Bahreyni <pbahr@email.arizona.edu>
2016-05-20 12:34:36 -0700

    Minor edits

--- week2.R
+++ week2.R
@@ -24,15 +24,37 @@ training <- spam[inTrain,]
 test <- spam[-inTrain,]
 
 set.seed(13343)
+
+# add capAve column with capitalAve values
 training$capAve <- training$capitalAve
+
+# turn some random values of capAve to NA
 selectNA <- rbinom(dim(training)[1], size= 1, prob = .05) == 1
-#sum(selectNA)/dim(training)[1]
-
 training$capAve[selectNA] <- NA
-names(training)
-preObj <- preProcess(training[,-58], method = "knnImpute")
-capAve <- predict(preObj, training[,-58])$capAve
 
-quantile(capAve - training$capitalAve)[!selectNA]
+str(training)
+names(training)
+# 59: capAve
+# 58: type
+# 55: capitalAve
+
+preObj <- preProcess(training[,-58], method = "knnImpute")
+
+#mine
+pre.df <- predict(preObj, training[,-58]) # works fine
+summary(pre.df$capAve)
+
+capAve <- pre.df$capAve
+# end of mine
+
+#capAve <- predict(preObj, training[,-58])$capAve
+
+quantile(capAve - training$capitalAve)
+length(capAve - training$capitalAve)
+
+length((capAve - training$capitalAve)[!selectNA])
+quantile((capAve - training$capitalAve)[!selectNA])
+
+quantile((capAve - training$capitalAve)[selectNA])
 
 str(Wage)
